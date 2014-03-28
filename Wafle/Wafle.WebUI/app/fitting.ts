f3d9d56f1be73b7fbee4a9b9be77ee255b0acad5
Shamna Skor <shamnaskor@outlook.com>
2014-03-28 12:49:08 -0400

    initial spike for data-driven menu

--- Wafle/Wafle.WebUI/app/fitting.ts
+++ Wafle/Wafle.WebUI/app/fitting.ts
@@ -12,11 +12,25 @@ ko.punches.enableAll();
 return {
     activate: function () {
         var that = this;
+        
     },
     attached: function () {
+        addRootWafleItemsToTwisty();
         $("div.twistyMenu li span").click((eventObject) => {
             var parent = (<Node>(eventObject.target)).parentNode;
             $(parent).find("li").toggleClass("twistyCollapse");
         });
     }
 };
+
+
+
+
+function addRootWafleItemsToTwisty() {
+    var rootMGs = Wafle.Data.WafleRootMarketGroups();
+    var htmlBuffer: string[] = [];
+    for (var i = 0; i < rootMGs.length; i++) {
+        htmlBuffer.push("<li><span>" + rootMGs[i].n + "</span><ul><li class='twistyCollapse'><span>item</span></li></ul></li>");
+    }
+    $("div.twistyMenu ul").append(htmlBuffer.join("\n"));
+}
