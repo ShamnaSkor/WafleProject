261f8bdee68b0a25b52fbd3b8a2a324e42dc6ea6
Shamna Skor <shamnaskor@outlook.com>
2014-03-28 18:46:47 -0400

    Patched Knockout.js to 3.1.0

--- Wafle/Wafle.WebUI/app/init.js
+++ Wafle/Wafle.WebUI/app/init.js
@@ -4,7 +4,7 @@
         "durandal": "../lib/Durandal/js",
         "plugins": "../lib/Durandal/js/plugins",
         "transitions": "../lib/Durandal/js/transitions",
-        "knockout": "../lib/knockout-3.0.0.debug",
+        "knockout": "../lib/knockout-3.1.0.debug",
         "jquery": "../lib/jquery",
         "jquery-ui": "../lib/jquery-ui",
         "handlebars": "../lib/handlebars-v1.3.0",
